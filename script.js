fbda3c35f36d5b55ee9a2f79f24e667c4352ac4d
InnovedaClub <its.innoveda@gmail.com>
2024-12-01 21:22:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const teamMembers = [
     { 
         name: 'Dr.M Karuppasamy', 
         role: 'Faculty Co-oridinator', 
-        image: '',
+        image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
         description:'',
         actions: { 
             linkedin: ' ', 
